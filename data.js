a9e0916e03f12d41cd7caf33e90d16ab43d34c59
uigy <jabko.tomasz@gmail.com>
2021-05-22 16:25:15 +0200

    Updates

---
+++ data.js
@@ -0,0 +1,1 @@
+const API="https://api.spacexdata.com/v4/";export const links=[{id:1,title:"capsules",API:`${API}capsules`,headers:["type","status"]},{id:2,title:"crew",API:`${API}crew`,headers:["name","agency"]},{id:3,title:"rockets",API:`${API}rockets`,headers:["name","company"]},{id:4,title:"starlink",API:`${API}starlink`,headers:["object name","launch date"]}];
